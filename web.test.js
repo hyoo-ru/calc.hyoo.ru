4d52b53671ef3a0b224573937d5722c781a52855
zerkalica <zerkalica@users.noreply.github.com>
2020-03-16 07:07:41 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.test.js
+++ web.test.js
@@ -89,6 +89,9 @@ var $;
 //test.test.js.map
 ;
 "use strict";
+//deep.js.map
+;
+"use strict";
 //deep.test.js.map
 ;
 "use strict";
@@ -1816,6 +1819,9 @@ var $;
 //view.test.js.map
 ;
 "use strict";
+//result.test.js.map
+;
+"use strict";
 var $;
 (function ($) {
     class $mol_style_sheet_test1 extends $.$mol_view {
@@ -1867,6 +1873,15 @@ var $;
                 }
             });
             $.$mol_assert_equal(sheet, '[mol_style_sheet_test] {\n\tflex-grow: 5;\n}\n');
+        },
+        'property shorthand'() {
+            class $mol_style_sheet_test extends $.$mol_view {
+            }
+            const { px } = $.$mol_style_unit;
+            const sheet = $.$mol_style_sheet($mol_style_sheet_test, {
+                padding: [0, 'auto']
+            });
+            $.$mol_assert_equal(sheet, '[mol_style_sheet_test] {\n\tpadding: 0 auto;\n}\n');
         },
         'component block styles with pseudo class'() {
             class $mol_style_sheet_test extends $.$mol_view {
@@ -2115,7 +2130,7 @@ var $;
         'buffer base64 decode'() {
             const str = 'GgoASUh42g==';
             const buffer = $.$mol_buffer.from(str, 'base64');
-            $.$mol_assert_like(buffer.original, new Uint8Array([26, 10, 0, 73, 72, 120, 218]));
+            $.$mol_assert_like(buffer.native, new Uint8Array([26, 10, 0, 73, 72, 120, 218]));
         },
         'buffer conform from same string are equal'() {
             const source = $.$mol_buffer.from('123');
