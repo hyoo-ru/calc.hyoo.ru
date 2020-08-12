023d1071a08d719f8d2121559c443d3b2e41ca96
zerkalica <zerkalica@users.noreply.github.com>
2020-08-12 07:12:53 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.js
+++ web.js
@@ -3628,6 +3628,12 @@ var $;
             }
             return found;
         }
+        size() {
+            switch (this.type()) {
+                case 'file': return this.stat().size;
+                default: return 0;
+            }
+        }
     }
     __decorate([
         $.$mol_mem
