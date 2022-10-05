e80c2e424d4d4dba181adcc0ecf55a9f44f680ee
nin-jin <nin-jin@users.noreply.github.com>
2022-10-05 07:36:01 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.esm.js
+++ web.esm.js
@@ -5590,6 +5590,9 @@ var $;
                 return val;
             return [];
         }
+        bring() {
+            return this.Edit().bring();
+        }
         Edit() {
             const obj = new this.$.$mol_textarea_edit();
             obj.value = (val) => this.value(val);
