a73221900a65f6af2d5f9f520438a769f1491692
nin-jin <nin-jin@users.noreply.github.com>
2023-04-24 07:16:22 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.js
+++ node.js
@@ -3460,7 +3460,7 @@ var $;
 //mol/page/page.view.css.ts
 ;
 "use strict";
-let $hyoo_sync_revision = "9edf754";
+let $hyoo_sync_revision = "697bb43";
 //hyoo/sync/-meta.tree/revision.meta.tree.ts
 ;
 "use strict";
@@ -4881,8 +4881,14 @@ var $;
         }
         async *delta(clocks = new Map()) {
             for (const land of this.lands.values()) {
-                yield await this.delta_batch(land, clocks.get(land.id()));
+                const batch = await this.delta_batch(land, clocks.get(land.id()));
+                if (batch.length)
+                    yield batch;
             }
+        }
+        async merge(donor) {
+            for await (const batch of donor.delta())
+                await this.apply(batch);
         }
         async apply(delta) {
             const units = [];
--- web.js
+++ web.js
@@ -2900,7 +2900,7 @@ var $;
 //mol/page/page.view.css.ts
 ;
 "use strict";
-let $hyoo_sync_revision = "9edf754";
+let $hyoo_sync_revision = "697bb43";
 //hyoo/sync/-meta.tree/revision.meta.tree.ts
 ;
 "use strict";
@@ -4491,8 +4491,14 @@ var $;
         }
         async *delta(clocks = new Map()) {
             for (const land of this.lands.values()) {
-                yield await this.delta_batch(land, clocks.get(land.id()));
+                const batch = await this.delta_batch(land, clocks.get(land.id()));
+                if (batch.length)
+                    yield batch;
             }
+        }
+        async merge(donor) {
+            for await (const batch of donor.delta())
+                await this.apply(batch);
         }
         async apply(delta) {
             const units = [];
