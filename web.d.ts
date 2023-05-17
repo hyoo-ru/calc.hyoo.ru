e4f705fbbb808525d14bae185d2142a9d6685568
nin-jin <nin-jin@users.noreply.github.com>
2023-05-17 07:15:30 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.d.ts
+++ web.d.ts
@@ -3037,6 +3037,12 @@ declare namespace $ {
 }
 
 declare namespace $ {
+    class $mol_icon_youtube extends $mol_icon {
+        path(): string;
+    }
+}
+
+declare namespace $ {
     class $mol_frame extends $mol_embed_native {
         dom_name(): string;
         attr(): Record<string, any>;
@@ -3074,6 +3080,7 @@ declare namespace $ {
         active(next?: any): boolean;
         title(): string;
         Image(): $$.$mol_image;
+        Hint(): $mol_icon_youtube;
         video_embed(): string;
         Frame(): $$.$mol_frame;
     }
@@ -3084,7 +3091,7 @@ declare namespace $.$$ {
         video_embed(): string;
         video_id(): string;
         video_preview(): string;
-        sub(): ($mol_image | $mol_frame)[];
+        sub(): $mol_frame[] | ($mol_image | $mol_icon_youtube)[];
     }
 }
 
