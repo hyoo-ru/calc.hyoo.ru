f2c673c164ef241fd364930636173c97db455946
nin-jin <nin-jin@users.noreply.github.com>
2025-10-18 07:21:46 +0000

    Deploying to gh-pages from @ hyoo-ru/calc.hyoo.ru@1cf1fa88bbef58b9e41eda2d2d7a703e0a300312 🚀

--- web.d.ts
+++ web.d.ts
@@ -1316,7 +1316,7 @@ declare namespace $ {
 }
 
 declare namespace $ {
-    function $mol_charset_decode(buffer: BufferSource, encoding?: $mol_charset_encoding): string;
+    function $mol_charset_decode(buffer: AllowSharedBufferSource, encoding?: $mol_charset_encoding): string;
 }
 
 declare var $node: any;
@@ -2860,11 +2860,16 @@ declare namespace $ {
 }
 
 declare namespace $ {
-    function $mol_base64_encode(src: string | Uint8Array<ArrayBuffer>): string;
+    function $mol_base64_url_encode(buffer: Uint8Array<ArrayBuffer>): string;
+    function $mol_base64_url_decode(str: string): Uint8Array<ArrayBuffer>;
 }
 
 declare namespace $ {
-    function $mol_base64_encode_web(str: string | Uint8Array<ArrayBuffer>): string;
+    function $mol_base64_encode(src: Uint8Array<ArrayBuffer>): string;
+}
+
+declare namespace $ {
+    function $mol_base64_encode_web(str: Uint8Array<ArrayBuffer>): string;
 }
 
 declare namespace $ {
@@ -2876,8 +2881,8 @@ declare namespace $ {
 }
 
 declare namespace $ {
-    function $mol_base64_url_encode(buffer: Uint8Array<ArrayBuffer>): string;
-    function $mol_base64_url_decode(str: string): Uint8Array<ArrayBuffer>;
+    function $mol_base64_url_encode_web(buffer: Uint8Array<ArrayBuffer>): string;
+    function $mol_base64_url_decode_web(str: string): Uint8Array<ArrayBuffer>;
 }
 
 declare namespace $ {
